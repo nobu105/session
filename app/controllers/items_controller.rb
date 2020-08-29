5677e3a80394fa5842aa8fe086f420bce5c7b251
nobu105 <dai9bensan@gmail.com>
2020-08-29 03:31:22 +0000

    deploy setting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,8 +22,22 @@ class ItemsController < ApplicationController
   def download
     @item = Item.find(params[:id])
     # ref: https://github.com/carrierwaveuploader/carrierwave#activerecord
-    filepath = @item.file.current_path
-    send_file(filepath.to_s, :filename => @item.file_identifier)
+    #filepath = "https://rails-item1.s3-ap-northeast-1.amazonaws.com/"+@item.file.current_path
+    #send_file(URI.encode(filepath.to_s))
+    #require 'aws-sdk'
+
+    #s3 = Aws::S3::Resource.new(region: 'ap-northeast-1')
+
+    # Create the object to retrieve
+    #obj = s3.bucket('rails-item1').object(@item.file.current_path)
+
+    # Get the item's content and save it to a file
+    #obj.get(response_target: './hoge')
+    #hoge = obj.download_file(@item.file.identifier)
+    #obj.get(response_target: "./"+@item.file.identifier)
+    filepath = "https://rails-item1.s3-ap-northeast-1.amazonaws.com/"+@item.file.current_path
+    data = open(URI.encode(filepath))
+    send_data data.read, filename: @item.file.identifier, type: "application/wav", disposition: 'inline', stream: 'true', buffer_size: '4096' 
   end
 
 
