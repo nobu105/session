4aab21c56fcdeecfb9d17a212c1236667380c396
nobu105 <dai9bensan@gmail.com>
2020-09-08 01:42:34 +0000

    about追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,9 +17,29 @@
 //= require_tree .
 //= require bootstrap-sprockets
 
+$(document).on('turbolinks:load', function() {
+  $(".inview").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-show");
+    }
+  });
+});
 
-
-
+$(document).on('turbolinks:load', function() {
+  $('a[href^="#"]').click(function(){
+    //スクロールのスピード
+    var speed = 1000;
+    //リンク元を取得
+    var href= $(this).attr("href");
+    //リンク先を取得
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    //リンク先までの距離を取得
+    var position = target.offset().top;
+    //スムーススクロール
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
 
 
 
