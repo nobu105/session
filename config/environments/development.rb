8d47ac9136ab683446afac1e96ceaacf6e2a831e
nobu105 <dai9bensan@gmail.com>
2020-09-13 07:02:41 +0000

    お問い合わせ修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,13 +60,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  #config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
+    :domain => "gmail.com",
     :user_name => ENV['MAIL_ADDRESS'],
     :password => ENV['MAIL_PASSWORD'],
     :authentication => :plain,
