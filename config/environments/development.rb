1efdf734edceed8384134b41be1cc0923284efac
nobu105 <dai9bensan@gmail.com>
2020-09-13 10:53:59 +0000

    bin修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,15 +63,27 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => ENV['MAIL_ADDRESS'],
-    :password => ENV['MAIL_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  #config.action_mailer.smtp_settings = {
+    #:address => "smtp.gmail.com",
+    #:port => 587,
+    #:user_name => ENV['MAIL_ADDRESS'],
+    #:password => ENV['MAIL_PASSWORD'],
+    #:authentication => :plain,
+    #:enable_starttls_auto => true
+  #}
 
+
+  config.action_mailer.smtp_settings = {
+      :enable_starttls_auto => true,
+      :address => 'smtp.gmail.com',
+      :port => 587,
+      :domain => 'gmail.com',
+      :authentication => :plain,
+      # メールアドレス(参照している)
+      :user_name => Rails.application.credentials.gmail[:mail_address],
+      # アプリパスワード(参照している)
+      :password => Rails.application.credentials.gmail[:app_password]
+  }
 
 end
 
