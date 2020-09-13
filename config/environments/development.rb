c8931767435c06c91a872ead19a219d5e71650cb
nobu105 <dai9bensan@gmail.com>
2020-09-13 06:54:44 +0000

    お問い合わせ修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['MAIL_ADDRESS'],
+    :password => ENV['MAIL_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
 
 end
